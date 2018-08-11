b72ac68a091d6288b9b4983023455af384bd76f7
OmegaANime <avik.kasi@mail.ru>
2018-08-11 03:41:30 -0700

    bla bla bla

---
+++ script.js
@@ -0,0 +1,29 @@
+function areArraysEqual(A,B){
+    for(var i = 0; i<A.length; i++){
+        if(!(A[i]=== B[i])){
+          return true;
+        }
+     }
+     return false;
+ }
+
+console.log(areArraysEqual([1,2,3], [1,2]));
+console.log(areArraysEqual([1,2,3], [1,4,3]));
+console.log(areArraysEqual([1,2,3], ["1",2,3]));
+console.log(areArraysEqual([1,2,3], [1,2,3]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
